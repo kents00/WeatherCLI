a695456a5c1dd03749763b69ca2be056f30f5add
Kent Edoloverio <kentedoloverio25@gmail.com>
2024-05-27 00:26:15 +0800

    Initial commit 5.27

--- sample.py
+++ sample.py
@@ -4,29 +4,49 @@ import datetime
 
 import asyncio
 import os
+import requests
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    city = response.get("city")
+    return city
+
 
 async def getweather():
-  # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
-  async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
-    # fetch a weather forecast from a city
-    weather = await client.get('Ozamiz')
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime('%Y-%m-%d %I:%M %p')
+    # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+    async with python_weather.Client(unit=python_weather.METRIC) as client:
+        # fetch a weather forecast from a city
+        location = get_location()
+        weather = await client.get(location)
 
-    # returns the current day's forecast temperature (int)
-    print(f"Date : {datetime.datetime.now()}")
-    print(f"Fahrenheit : {weather.temperature}")
+        # Print current location, date and temperature
+        print(f"Location : {location}")
+        print(f"Date : {formatted_datetime}")
+        print(f"Celcius : {weather.temperature}")
 
-    # get the weather forecast for a few days
-    for daily in weather.daily_forecasts:
-      print(daily)
-
-      # hourly forecasts
-      for hourly in daily.hourly_forecasts:
-        print(f' --> {hourly!r}')
+        # Print the forecast for tomorrow
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+        print("\nTomorrows forecast:")
+        for daily in weather.daily_forecasts:
+            if daily.date == tomorrow:
+                print(f"Temperature: {daily.temperature}")
+                for hourly in daily.hourly_forecasts:
+                    print(
+                        f"Time: {hourly.time},\nTemperature: {hourly.temperature},\nDescription: {hourly.description},\nKind: {hourly.kind}")
+                break
 
 if __name__ == '__main__':
-  # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
-  # for more details
-  if os.name == 'nt':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
+    # for more details
+    if os.name == 'nt':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-  asyncio.run(getweather())
+    asyncio.run(getweather())
